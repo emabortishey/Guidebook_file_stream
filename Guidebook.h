6b1c08df2fc0624b3d2b7ded31240538df0e5ab3
fartb <fartbreather228@gmail.com>
2025-01-09 01:23:04 +0300

    созданы ещё 3 метода поиска по имени. номеру и по роду деятельности. тесты не проведены, ведь коды практически идентичны с кодом в первом методе за исключением 1 слова и названия

--- Guidebook.h
+++ Guidebook.h
@@ -101,6 +101,84 @@ public:
 		return buff;
 	}
 
+	Guidebook search_by_owner(string obj)
+	{
+		Guidebook buff;
+		ifstream testfile;
+		int count = 0;
+
+		testfile.open("Info.txt");
+
+		while (count == 0)
+		{
+			testfile >> buff.Company_name;
+			testfile >> buff.Owner;
+			testfile >> buff.Number;
+			testfile >> buff.Adress;
+			testfile >> buff.Occupation;
+			if (buff.Owner == obj)
+			{
+				count++;
+			}
+		}
+
+		testfile.close();
+
+		return buff;
+	}
+
+	Guidebook search_by_number(string obj)
+	{
+		Guidebook buff;
+		ifstream testfile;
+		int count = 0;
+
+		testfile.open("Info.txt");
+
+		while (count == 0)
+		{
+			testfile >> buff.Company_name;
+			testfile >> buff.Owner;
+			testfile >> buff.Number;
+			testfile >> buff.Adress;
+			testfile >> buff.Occupation;
+			if (buff.Number == obj)
+			{
+				count++;
+			}
+		}
+
+		testfile.close();
+
+		return buff;
+	}
+
+	Guidebook search_by_occupation(string obj)
+	{
+		Guidebook buff;
+		ifstream testfile;
+		int count = 0;
+
+		testfile.open("Info.txt");
+
+		while (count == 0)
+		{
+			testfile >> buff.Company_name;
+			testfile >> buff.Owner;
+			testfile >> buff.Number;
+			testfile >> buff.Adress;
+			testfile >> buff.Occupation;
+			if (buff.Occupation == obj)
+			{
+				count++;
+			}
+		}
+
+		testfile.close();
+
+		return buff;
+	}
+
 	string get_comp() { return Company_name; }
 	string get_owner() { return Owner; }
 	string get_num() { return Number; }
